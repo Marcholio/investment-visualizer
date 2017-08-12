952fed2a6492c1c467f4c24a36c0da0a150cfcc3
markus <marcholio09@gmail.com>
2017-08-12 18:01:41 +0300

    Cors anywhere

--- src/proxy.js
+++ src/proxy.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const request = require('request');
-const cors = require('cors');
+const cors = require('cors-anywhere');
 
-const app = express();
-app.use(cors());
-
-app.options('*');
-app.get('*', (req, res) => {
-  req.pipe(request(`http:/${req.url}`)).pipe(res);
-});
-
-/* eslint-disable no-console */
-app.listen(8080, () => console.log('Proxy started'));
+cors.createServer({}).listen(8080, () => console.log('Proxy running'));
